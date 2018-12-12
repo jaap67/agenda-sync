cdd1aa92704987c2ba253131e652260921acb286
Jose Arlan <ze_harlan@me.com>
2018-12-12 09:44:15 -0300

    syncronizacao do servidor com firebase

--- app/src/main/java/br/com/alura/agenda/firebase/AgendaInstanceIDService.java
+++ app/src/main/java/br/com/alura/agenda/firebase/AgendaInstanceIDService.java
@@ -5,6 +5,11 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import br.com.alura.agenda.retrofit.RetrofitInicializador;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AgendaInstanceIDService extends FirebaseInstanceIdService {
 
     @Override
@@ -19,7 +24,19 @@ public class AgendaInstanceIDService extends FirebaseInstanceIdService {
         enviaTokenParaServidor(refreshedToken);
     }
 
-    private void enviaTokenParaServidor(String refreshedToken) {
+    private void enviaTokenParaServidor(final String token) {
+        Call<Void> call = new RetrofitInicializador().getDispositivoService().enviaToken(token);
 
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Log.i("token enviado", token);
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.e("token falhou", t.getMessage());
+            }
+        });
     }
 }
---
+++ app/src/main/java/br/com/alura/agenda/firebase/AgendaMessagingService.java
@@ -0,0 +1,19 @@
+package br.com.alura.agenda.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+public class AgendaMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Map<String, String> mensagem = remoteMessage.getData();
+        Log.i("mensagem recebida", String.valueOf(mensagem));
+    }
+}
