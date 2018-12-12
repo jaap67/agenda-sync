107b42986f97196f73791d7c86b000260067a046
Jose Arlan <ze_harlan@me.com>
2018-12-12 13:04:16 -0300

    adicionando eventBus

--- app/src/main/java/br/com/alura/agenda/firebase/AgendaMessagingService.java
+++ app/src/main/java/br/com/alura/agenda/firebase/AgendaMessagingService.java
@@ -2,10 +2,18 @@ package br.com.alura.agenda.firebase;
 
 import android.util.Log;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.greenrobot.eventbus.EventBus;
+
+import java.io.IOException;
 import java.util.Map;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.dto.AlunoSync;
+import br.com.alura.agenda.event.AtualizaListaAlunoEvent;
 
 public class AgendaMessagingService extends FirebaseMessagingService {
 
@@ -15,5 +23,25 @@ public class AgendaMessagingService extends FirebaseMessagingService {
 
         Map<String, String> mensagem = remoteMessage.getData();
         Log.i("mensagem recebida", String.valueOf(mensagem));
+
+        converteParaAluno(mensagem);
+    }
+
+    private void converteParaAluno(Map<String, String> mensagem) {
+        String chaveDeAcesso = "alunoSync";
+        if (mensagem.containsKey(chaveDeAcesso)){
+            String json = mensagem.get(chaveDeAcesso);
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                AlunoSync alunoSync = mapper.readValue(json, AlunoSync.class);
+                AlunoDAO dao = new AlunoDAO(this);
+                dao.sincroniza(alunoSync.getAlunos());
+                dao.close();
+                EventBus eventBus = EventBus.getDefault();
+                eventBus.post(new AtualizaListaAlunoEvent());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
